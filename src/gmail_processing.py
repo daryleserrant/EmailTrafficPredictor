c4e5f9f96d984155bbd39845636a86ed520d3ddd
daryleserrant <djserrant1@gmail.com>
2016-06-23 15:23:49 -0700

    Adding Hourly Arima Model Building Code

--- src/gmail_processing.py
+++ src/gmail_processing.py
@@ -1,6 +1,79 @@
 import pandas as pd
 import numpy as np
 import cPickle as pickle
+from multiprocessing import Pool
+from functools import partial
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+from multiprocessing import Pool
+from operator import itemgetter
+from datetime import datetime
+import pytz
+
+HOURLY_PERIOD = 24
+
+'''{'aic': 13503.050049614645,
+ 'params': {'D': 1, 'P': 1, 'Q': 2, 'd': 1, 'p': 0, 'q': 10}}'''
+
+DEFAULT_HOURLY_PARAMS = {'p':0, 'd':1,'q': 24,'P':1,'D':1,'Q':2}
+DAILY_PERIOD = 365
+
+def run_grid_search(data, params):
+    '''
+    Perform grid search on seasonal arima model
+    
+    Arguments:
+        data - the time series data
+        params - a list of SARIMAX parameters. (A list of lists)
+    
+    Returns:
+        A dictionary with the optimal aic and parameters
+    '''
+    best_aic = np.inf
+    best_params = None
+    
+    for pm in params:
+        p,d,q,P,D,Q = tuple(pm)
+        model = SARIMAX(data, order=(p,d,q), seasonal_order=(P,D,Q,HOURLY_PERIOD), simple_differencing=True,enforce_stationarity=False,
+        enforce_invertibility=False).fit()
+        print 'p:{},d:{},q:{},P:{},D:{},Q:{}, AIC:{}'.format(p,d,q,P,D,Q,model.aic)
+        if model.aic < best_aic:
+            best_aic = model.aic
+            best_params = {'p':p,'d':d,'q':q, 'P':P,'D':D, 'Q':Q}
+
+    return {'params':best_params,'aic':best_aic}
+
+def parallel_grid_search_arima(ts, params):
+    '''
+    Perform parallel grid searches on arima model
+    
+    Arguments:
+        ts - the time series
+        params - a dictionary of SARIMAX parameters
+    
+    Returns:
+        The parameters that produces most optimal aic 
+    '''
+    pool = Pool(processes=4)
+    p_list = []
+    best_aic = np.inf
+    best_params = None
+    
+    partial_srch = partial(run_grid_search,ts)
+    
+    for lp in params['p']:
+        for lq in params['q']:
+            for ld in params['d']:
+                for bp in params['P']:
+                    for bd in params['D']:
+                        for bq in params['Q']:
+                            if bp == 0 and bq == 0:
+                                continue
+                            p_list.append([lp,ld,lq,bp,bd,bq])
+    results = pool.map(partial_srch, [p_list])
+    
+    sorted_results = sorted(results, key=itemgetter('aic'))
+
+    return sorted_results[0]
 
 def get_unique_labels(data):
     '''
@@ -131,7 +204,7 @@ def messages_to_dataframe(messages):
             snippet.append(m['snippet'])
             thread_id.append(m['threadId'])
     
-    df_emails = pd.DataFrame(
+    df = pd.DataFrame(
     {
         'history_id': history_id,
         'msg_id': msg_id,
@@ -148,13 +221,59 @@ def messages_to_dataframe(messages):
         'thread_id' : thread_id
     })
     
-    msg_labels = get_unique_labels(df_emails['label_ids'])
+    msg_labels = get_unique_labels(df['label_ids'])
     
     for lbl in msg_labels:
-       df_emails['is_'+lbl.lower()] = has_label(df_emails['label_ids'], lbl)
+       df['is_'+lbl.lower()] = has_label(df['label_ids'], lbl)
     
-    return df_emails.drop('label_ids', axis=1)
+    df['year'] = df['internal_date'].apply(lambda x: time.gmtime(int(x)/1000).tm_year)
+    df['month'] = df['internal_date'].apply(lambda x: time.gmtime(int(x)/1000).tm_mon)
+    df['day'] = df['internal_date'].apply(lambda x: time.gmtime(int(x)/1000).tm_mday)
+    df['hour'] = df['internal_date'].apply(lambda x: time.gmtime(int(x)/1000).tm_hour)
+    df['min'] = df['internal_date'].apply(lambda x: time.gmtime(int(x)/1000).tm_min)
+    df['sec'] = df['internal_date'].apply(lambda x: time.gmtime(int(x)/1000).tm_sec)
+    df['wday'] = df['internal_date'].apply(lambda x: time.gmtime(int(x)/1000).tm_wday)
+    df['yday'] = df['internal_date'].apply(lambda x: time.gmtime(int(x)/1000).tm_yday)
+    df['isdst'] = df['internal_date'].apply(lambda x: time.gmtime(int(x)/1000).tm_isdst)
+    df['date'] = df['internal_date'].apply(lambda x: datetime.fromtimestamp(int(x)/1000,tz=pytz.utc))
+    
+    return df.drop('label_ids', axis=1)
 
+def build_hourly_arima_model(ts, params=None):
+    '''
+    Builds an arima model that forecast hourly email traffic.
+    
+    Arguments:
+        ts - a time series
+        params - parameters to pass to the SARIMAX training algorithms
+    
+    Returns:
+        an arima model
+    '''
+    hourly_agg = ts[['year','month','day','hour','msg_id']].groupby(['year','month','day','hour']).count()
+    
+    hourly_index = pd.date_range(rcvd_messages['date'].min().floor('H'), rcvd_messages['date'].max().floor('H'), freq='H')
+    hourly_counts = pd.Series(0, index=hourly_index)
+    
+    for dt in hourly_index:
+    try:
+        hourly_counts[dt] = hourly_agg.ix[dt.year,dt.month,dt.day,dt.hour]
+    except:
+        hourly_counts[dt] = 0
+    
+    if params == None:
+       params = DEFAULT_HOURLY_PARAMS
+    
+    p = params['p']
+    d = params['d']
+    q = params['q']
+    P = params['Q']
+    D = params['D']
+    Q = params['Q']
+    
+    return SARIMAX(train, order=(p,d,q), seasonal_order=(P,D,Q,HOURLY_PERIOD), simple_differencing=True,enforce_stationarity=False,
+         enforce_invertibility=False).fit()
+         
 if __name__ == '__main__':
 
     with open('threads.pkl', 'r') as f:
