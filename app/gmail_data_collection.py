41008df20029dea1ef8769af06304449492a431d
daryleserrant <djserrant1@gmail.com>
2016-06-25 07:24:23 -0700

    Restructing code, prototype gmail data pipeline job scheduling functions

---
+++ app/gmail_data_collection.py
@@ -0,0 +1,162 @@
+from apiclient.discovery import build
+from apiclient.http import BatchHttpRequest
+from httplib2 import Http
+from oauth2client import file, client, tools
+import sys
+import cPickle as pickle
+
+MAX_CALLS_PER_REQUEST = 1000  # The Google API limits us to 1000 calls in a single batch request!
+                              # This variable should never exceed 1000. 
+
+emails = []
+
+def add_email_message(request_id, response, exception):
+    '''
+    Callback function for message batch requests performed in request_message
+    function below. Store the request response in a list
+    
+    Arguments:
+       request_id - The id of the request
+       response - The result obtained from the API call request
+       exception - The exception thrown from the API, if any occurred
+    '''
+    emails.append(response)
+     
+def create_service():
+    '''
+    Creates a GMAIL API service object using stored user credentials. If nothing has been
+    stored or the stored credentials are invalid, obtain new credentials from the user.
+    
+    Returns:
+        The GMAIL API service object
+    '''
+    SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
+    CLIENT_SECRET = 'client_secret.json'
+    
+    store = file.Storage('storage.json')
+    creds = store.get()
+    
+    if creds is None or creds.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET, SCOPES)
+        creds = tools.run(flow,store)
+    
+    GMAIL = build('gmail','v1', http=creds.authorize(Http()))
+    return GMAIL
+
+def request_threads(service):
+    '''
+    Retrieves all message threads from the user's GMAIL account.
+    
+    Arguments:
+        service - A GMAIL API Service object
+    Returns:
+        A list of message threads (defined as dictionaries)
+    '''
+    response = service.users().threads().list(userId='me').execute()
+    threads = []
+    
+    print "Requesting message threads..."
+    
+    if 'threads' in response:
+        threads.extend(response['threads'])
+
+    while 'nextPageToken' in response:
+        page_token = response['nextPageToken']
+        response = service.users().threads().list(userId='me',pageToken=page_token).execute()
+        threads.extend(response['threads'])
+    
+    return threads
+
+def request_message_ids(service, after_date = None):
+    '''
+    Retrieves all message ids from the user's GMAIL account.
+    
+    Arguments:
+        service - A GMAIL API Service object
+    Returns:
+        A list of dictionaries that contain the id of a single message
+    '''
+    
+    query = None
+    if after_date:
+        query = 'after:{}'.format(after_date.strftime('%Y/%m/%d')
+    
+    response = service.users().messages().list(userId='me',q=query)execute()
+    messages = []
+    
+    print "Requesting message ids..."
+    
+    if 'messages' in response:
+        messages.extend(response['messages'])
+    
+    while 'nextPageToken' in response:
+        page_token = response['nextPageToken']
+        response = service.users().messages().list(userId='me',q=query, pageToken=page_token).execute()
+        messages.extend(response['messages'])
+    
+    return messages
+
+def request_messages(service, messages):
+    '''
+    Executes one or more batch requests in order to retrieve every message in the 
+    user's GMAIL account.
+    
+    Arguments:
+        service  - A GMAIL API Service object
+        messages - A list of dictionaries that contain the id of a single message 
+    Returns:
+        A list of messages (defined as dictionaries)
+    '''    
+    curr = -1
+    batch_requests = []
+    global emails
+    emails.append(1)
+    
+    print "Creating message batch requests..."
+    for i, msg in enumerate(messages):
+        if i % MAX_CALLS_PER_REQUEST == 0:
+            batch_requests.append(service.new_batch_http_request())
+            curr += 1
+        else:
+            batch_requests[curr].add(service.users().messages().get(userId='me', id = msg['id']),
+                    callback=add_email_message)
+    
+    batch_size = len(batch_requests)
+    for i, batch in enumerate(batch_requests):
+        print "Executing batch request {} of {}...".format(i+1, batch_size)
+        batch.execute()
+
+def collect_messages(after_date):
+    service = create_service()
+    
+    message_ids = request_message_ids(service, after_date)
+    
+    # Empty list
+    emails = []
+    
+    request_messages(service, message_ids)
+    
+    return emails.copy()
+    
+        
+if __name__ == '__main__':
+    service = create_service()
+    
+    threads = request_threads(service)
+    
+    print "Saving threads.pkl"
+    with open('threads.pkl' 'w') as f:
+        pickle.dump(threads,f)
+    
+    message_ids = request_message_ids(service)
+    
+    print "Saving message_ids.pkl"
+    with open('message_ids.pkl' 'w') as f:
+        pickle.dump(threads,f)    
+    
+    request_messages(service, message_ids)
+    
+    print "Saving emails.pkl"
+    with open('emails.pkl', 'w') as f:
+        pickle.dump(emails,f)
+    
