13f81e79fba94903c6261cb02922e1c9d0bcc42a
daryleserrant <djserrant1@gmail.com>
2016-06-30 16:45:19 -0700

    Added comments to forecaster objects

--- app/gmail_traffic_forecaster.py
+++ app/gmail_traffic_forecaster.py
@@ -7,6 +7,9 @@ from datetime import datetime, timedelta
 import dateutil.relativedelta as relativedelta
 
 class Forecaster(object):
+'''
+A base time series forecasting class
+'''
     
     def __init__(self):
         pass
@@ -18,6 +21,10 @@ class Forecaster(object):
         pass
 
 class DailyForecaster(Forecaster):
+'''
+A Forecaster subclass that forecasts the daily email traffic using the SARIMAXResults
+class from statsmodels
+'''
     
     def __init__(self, model=None):
         self.model = model
@@ -29,7 +36,10 @@ class DailyForecaster(Forecaster):
         self.model = SARIMAXResults.load(filepath)
         
 class HourlyForecaster(Forecaster):
-
+'''
+A Forecaster subclass that forecasts the hourly email traffic using the holtwinters
+additive exponential smoothing algorithm.
+'''
     def __init__(self, alpha=None, beta=None, gamma=None, period=None, ts=None):
         self.alpha = alpha
         self.beta = beta
